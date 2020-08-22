895e577015998b251fef2700c2ce5d3489e78bd6
simonkurek <simonkurek@protonmail.com>
2020-08-22 23:40:35 +0200

    update

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -8,21 +8,39 @@ const SRV = SRV_PROTOCOL + '://' + SRV_IP + ':' + SRV_PORT
 
 /* -- app -- */
 
+//article loading func
+const loadArticle = (id) => {
+    fetch(SRV + '/getByID?id=' + id)
+        .then(resp => resp.json())
+        .then(article => {
+            console.log(article)
+            let html = '<article>'
+            html += '<h2>' + article.title + '</h2>'
+            html += '<h4> Kategoria: ' + article.category + ' | Autor: ' + article.author + '</h4>'
+            html += '<p>' + article.content + '</p>'
+            html += '</article>'
+            document.getElementById('articles').innerHTML = html
+        })
+}
+
 //initial func
 const init = () => {
     fetch(SRV + '/getAll')
         .then(resp => resp.json())
         .then(resp => {
             console.log(resp)
-            let html = '<article>'
+
             resp.forEach(article => {
+                let html = '<article>'
                 html += '<h2>' + article.title + '</h2>'
                 html += '<h4> Kategoria: ' + article.category + ' | Autor: ' + article.author + '</h4>'
                 html += '<h3>' + article.description + '</h3>'
-                html += '<a href="#" class="button">Czytaj dalej</a>'
+                html += '<a href="#" class="button" id="art' + article.id + '">Czytaj dalej</a>'
                 html += '</article>'
+                document.getElementById('articles').innerHTML += html
+                document.getElementById('art' + article.id)
+                    .addEventListener('click', _ => loadArticle(article.id))
             })
-            document.getElementById('articles').innerHTML = html
         })
 }
 
